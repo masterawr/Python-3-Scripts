9e1effff1efc0f60a40bc33fd28b35a974836272
masterawr <53235558+masterawr@users.noreply.github.com>
2019-07-24 14:59:07 -0400

    Update combine_csv.py

--- combine_csv.py
+++ combine_csv.py
@@ -2,16 +2,12 @@
 import os
 import glob
 import pandas as pd
-
-#Change “/mydir” to your desired working directory
+#sets the working directory
 os.chdir("C:\workspace")
-
-#Match the pattern (‘csv’) and save the list of file names in the ‘all_filenames’ variable
+#gathers all files with the extension csv
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-
-#combine all files in the list
+#combine all files in the working directory
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-
-#export to csv
+#export to combined_csv.csv
 combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
