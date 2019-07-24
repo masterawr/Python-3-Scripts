ed2a49ec0bb31d9ca9b1914aca06f65a0a4ee2c1
masterawr <53235558+masterawr@users.noreply.github.com>
2019-07-24 14:54:11 -0400

    Update csv_rearrange_col.py

--- csv_rearrange_col.py
+++ csv_rearrange_col.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import pandas as pd
-
+#sets the working directory
 os.chdir("C:\workspace")
+#reads the csv file
 df = pd.read_csv("combined_csv.csv")
-#re-arrange columns in the order ytou want
+#re-arrange columns and outputs it in a file called outfile.csv
 df.reindex(columns=["Title 3", "Title 1", "Title 2"]).to_csv('outfile.csv', index=False,header=True)
