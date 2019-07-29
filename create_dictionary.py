a67b03a488b51cf31f06a3faf3094c7f6cddec51
masterawr <53235558+masterawr@users.noreply.github.com>
2019-07-29 13:24:12 -0400

    Add files via upload
    This creates a python dictionary from a CSV file

---
+++ create_dictionary.py
@@ -0,0 +1,22 @@
+#This creates a python dictionary from a CSV file 
+import csv
+import os
+
+#sets the working directory
+os.chdir("C:\workspace")
+
+#first column of the csv file contains unique keys and the second column contains values
+with open('my_file.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    with open('dictionary.csv', mode='w') as outfile:
+        writer = csv.writer(outfile)
+        mydict = {rows[0]:rows[1] for rows in reader} #mydict is the new dictionary
+
+#print to commnad line in column format
+for k, v in mydict.items():
+    print (f'{k:<4} {v}')
+    
+#write dictionary to CSV file
+w = csv.writer(open('dictionary.csv', 'w'))
+for key, val in mydict.items():
+    w.writerow([key, val])
